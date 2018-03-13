ae73846069b2e4f7d7521b007e182570559762cb
Xcode User <xcode@fasts-macbook-pro.local>
2018-03-13 13:03:57 -0400

    Initial Commit

---
+++ Barcode Card Scanner/ViewControllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Barcode Card Scanner
+//
+//  Created by Xcode User on 2018-03-13.
+//  Copyright © 2018 Team SJM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToMenu(segue : UIStoryboardSegue){
+        
+    }
+
+}
